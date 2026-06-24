3b04234f73c58f31afb172db0382c9061dcc22cd
seoupark1 <seoupark05@gmail.com>
2026-06-24 15:26:21 +0000

    MRP to DCM, principal inertias와 dcm 출력하는 함수 추가

--- utils/utils_kinematics.py
+++ utils/utils_kinematics.py
@@ -8,6 +8,7 @@ def tilde(v):
 
     return result
 
+
 # (3-2-1) Euler Angles to Direction Cosine Matrix
 def EA2C_321(EA_angles_deg):
     # deg to rad
@@ -25,6 +26,7 @@ def EA2C_321(EA_angles_deg):
 
     return dcm_BN
 
+
 # (3-1-3) Euler Angles to Direction Cosine Matrix
 def EA2C_313(EA_angles_deg):
     # deg to rad
@@ -41,3 +43,30 @@ def EA2C_313(EA_angles_deg):
                        [s2*s1, -s2*c1, c2]])
     
     return dcm_BN
+
+
+# Modified Rodrigues Parameters to Direction Cosine Matrix
+def MRP2C(sigma):
+    dcm_BN = (np.eye(3) + (8 * tilde(sigma) @ tilde(sigma) - 4 * (1 - np.vdot(sigma,sigma)) * tilde(sigma)) / (1 + np.vdot(sigma,sigma))**2)
+
+    return dcm_BN
+
+
+# Principal Inertias (descending order) & Corresponding DCM
+def coordinate_transform(Ic_B):
+    # get eigenvalues & eigenvectors
+    eig_vals, eig_vecs = np.linalg.eigh(Ic_B)
+
+    # change eigenvalue's index
+    I_min, I_med, I_max = eig_vals[0], eig_vals[1], eig_vals[2]
+    eig_vals = np.array([I_max, I_med, I_min])
+
+    # change eigenvector's index & make [FB]
+    v_min, v_med, v_max = eig_vecs[:, 0], eig_vecs[:, 1], eig_vecs[:, 2]
+    dcm_FB = np.array([v_max, v_med, v_min])
+
+    # if dcm_FB's det = -1, change it to follow right-handed rule
+    if np.linalg.det(dcm_FB) < 0:
+        dcm_FB[2, :] = -dcm_FB[2, :]
+
+    return eig_vals, dcm_FB
